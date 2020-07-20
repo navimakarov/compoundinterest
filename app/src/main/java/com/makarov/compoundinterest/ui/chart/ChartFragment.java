7b240721e9ce7f3c126bb8a0ae592983e4a30630
navimakarov <makarov18042003@gmail.com>
2020-07-20 12:13:44 +0300

    Fixed chart on huge numbers

--- app/src/main/java/com/makarov/compoundinterest/ui/chart/ChartFragment.java
+++ app/src/main/java/com/makarov/compoundinterest/ui/chart/ChartFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,20 +17,18 @@ import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.utils.ColorTemplate;
 import com.makarov.compoundinterest.R;
 import com.makarov.compoundinterest.ui.DataHolderClass;
 
+import org.w3c.dom.Text;
+
 import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.util.ArrayList;
 
 public class ChartFragment extends Fragment {
 
     private ChartViewModel dashboardViewModel;
 
-
-    View root;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -39,10 +38,14 @@ public class ChartFragment extends Fragment {
 
         BarChart barChart = root.findViewById(R.id.barChart);
         ArrayList<BarEntry> balances = new ArrayList<>();
+        ScrollView scrollView = (ScrollView) root.findViewById(R.id.scrollView);
+        TextView noData = (TextView) root.findViewById(R.id.nodata);
 
         try {
             String[] data = DataHolderClass.getInstance().getDistributor_id().split(",");
             if (data.length >= 4) {
+                noData.setVisibility(View.INVISIBLE);
+                scrollView.setVisibility(View.VISIBLE);
                 long startingBalance = Long.parseLong(data[0]);
                 long monthlyContribution = Long.parseLong(data[1]);
                 double interestRate = Double.parseDouble(data[2]);
@@ -50,31 +53,36 @@ public class ChartFragment extends Fragment {
 
                 balances.clear();
                 BigDecimal capital = new BigDecimal(String.valueOf(startingBalance));
-                String divider = "1";
+                long chartValue;
+                ArrayList<BigDecimal> bigDecimal_capital = new ArrayList<>();
                 for(int i = 0; i < duration; i++) {
                     capital = capital.add(capital.multiply(new BigDecimal(String.valueOf(interestRate))));
                     capital = capital.add(new BigDecimal(String.valueOf(monthlyContribution * 12)));
-                    capital = capital.divide(new BigDecimal(divider));
 
-                    long chartValue = capital.longValue();
-                    if(chartValue < 0){
-                        balances.clear();
-                        divider += "000";
-                        capital = new BigDecimal(String.valueOf(startingBalance));
-                        i = 0;
-                    }
-                    else {
-                        balances.add(new BarEntry(i + 1, chartValue));
-                    }
+                    bigDecimal_capital.add(capital);
+                    //chartValue = capital.longValue();
+                    //balances.add(new BarEntry(i + 1, chartValue));
+                }
+                while(bigDecimal_capital.get((int)duration-1).compareTo(new BigDecimal(Long.MAX_VALUE)) == 1){
+                    for(int i = 0; i < duration; i++){
+                        BigDecimal old_value = bigDecimal_capital.get(i);
+                        BigDecimal new_value = old_value.divide(new BigDecimal("100"));
+                        bigDecimal_capital.set(i, new_value);
 
+                    }
+                }
+
+                for(int i = 0; i < duration; i++){
+                    chartValue = bigDecimal_capital.get(i).longValue();
+                    balances.add(new BarEntry(i+1, chartValue));
                 }
             }
 
         }
 
         catch (Exception e){
-
-
+            noData.setVisibility(View.VISIBLE);
+            scrollView.setVisibility(View.INVISIBLE);
         }
 
         finally {
